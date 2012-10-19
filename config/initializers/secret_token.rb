cbc3f1666c7dfaa45be4e513a65ac16c44ae2d28
Ryo Matsumoto <bussorenre0088@gmail.com>
2012-10-19 23:35:20 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minibbs::Application.config.secret_token = '93cf935840b7caebe3f30f3c49f5cab497f5100bc07b637593b8aba9a6022aa92fdb254ec530e8a0847f0a69625f11bca988a6c06107b0002409632b864ff8bc'
